97dbcfe3c54b129075e3b01d1e2c086c1b12d0fb
Leticia Dias <ledias@uolinc.com>
2020-05-31 19:59:30 -0300

    Add basic table controller for reference

---
+++ src/controllers/tables.controller.ts
@@ -0,0 +1,30 @@
+import express from 'express'
+import { Request, Response } from 'express'
+
+import IControllerBase from '../interfaces/IControllerBase.interface'
+
+class TablesController implements IControllerBase {
+    public path = '/tables';
+    public router = express.Router();
+
+    constructor () {
+        this.initRoutes();
+    }
+
+    public initRoutes() {
+        this.router
+            .get('/', this.getAll);
+    }
+
+    getAll = (req: Request, res: Response) => {
+        const tables = [{
+            id: 1,
+            number: 10,
+            clientsCount: 3
+        }];
+
+        res.send(tables);
+    }
+}
+
+export default TablesController
