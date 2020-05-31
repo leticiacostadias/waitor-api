2a10d3f686091473c8a7f5e2a1c85e5ae51f7d31
Leticia Dias <ledias@uolinc.com>
2020-05-31 19:59:06 -0300

    Add App class and its call on index.ts

---
+++ src/App.ts
@@ -0,0 +1,41 @@
+import express from "express";
+
+import { PORT } from "./config";
+
+class App {
+  public app: express.Application;
+  public port: number;
+
+  constructor(appInit: { middleWares: any; controllers: any }) {
+    this.app = express();
+    this.port = PORT;
+
+    this.middlewares(appInit.middleWares);
+    this.routes(appInit.controllers);
+  }
+
+  private middlewares(middleWares: {
+    forEach: (arg0: (middleWare: any) => void) => void;
+  }) {
+    middleWares.forEach((middleWare) => {
+      this.app.use(middleWare);
+    });
+  }
+
+  private routes(controllers: {
+    forEach: (arg0: (controller: any) => void) => void;
+  }) {
+    controllers.forEach((controller) => {
+      this.app.use(controller.path, controller.router);
+    });
+  }
+
+  public listen() {
+    this.app.listen(this.port, () => {
+      // tslint:disable-next-line:no-console
+      console.log(`server started at http://localhost:${this.port}`);
+    });
+  }
+}
+
+export default App;
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
-import express from 'express';
-const app = express();
-const port = 8080; // default port to listen
+import bodyParser from "body-parser";
 
-// define a route handler for the default home page
-app.get("/", (req, res) => {
-  res.send("Hello world!");
+import App from "./App";
+
+import TablesController from './controllers/tables.controller';
+
+const app = new App({
+  controllers: [
+    new TablesController()
+  ],
+  middleWares: [
+    bodyParser.json(),
+    bodyParser.urlencoded({ extended: true })
+  ],
 });
 
-//
-// start the Express server
-app.listen(port, () => {
-  // tslint:disable-next-line:no-console
-  console.log(`server started at http://localhost:${port}`);
-});
+app.listen();
